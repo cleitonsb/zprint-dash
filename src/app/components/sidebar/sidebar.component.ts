8f20ee6bc15c56f91250dec7e174a7a0a8ba6f97
Cleiton Barbosa <cleitonsbarbosa@gmail.com>
2021-04-25 17:15:32 -0300

    [FEATURE] Ponto

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -34,7 +34,7 @@ export class SidebarComponent implements OnInit {
 
   menuItems: any[];
   isCollapsed = true;
-  version = '0.10';
+  version = '0.11';
 
   constructor(private router: Router, private service: UserService, private emitterService: EmitterService ) { }
 
--- src/app/modules/clocks/clock-list/clock-list.component.ts
+++ src/app/modules/clocks/clock-list/clock-list.component.ts
@@ -1,9 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 import { first } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
 import { ClockService  } from 'src/app/services/clock.service';
+import { UserService } from 'src/app/services/user.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -25,48 +29,106 @@ export class ClockListComponent implements OnInit {
   zoom: number = 15;
   usuario: any;
   periodo: any;
+  user = new User();
+  totalPonto: number = 0;
+  totalMes: number = 0;
 
   constructor(
     private service: ClockService,
+    private userService: UserService,
     private spinner: NgxSpinnerService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-
-    console.log(1);
-
     this.spinner.show();
     this.route.params.subscribe((params) => {
+      this.getUsuario(params);      
+    });
+  }
 
+  getUsuario(params) {
+    this.userService.get(params.usuario).subscribe((data: any) => {
+      this.user = data;
 
-      var usuario = params.usuario.split('-');
-
-      this.funcionario = usuario[1];
-      var idUsuario = usuario[0];
-
-      this.getRegistros(idUsuario, params.periodo);
+      this.getRegistros(params.usuario, params.periodo);
     });
   }
 
   getRegistros(usuario, periodo) {
 
+    if(!periodo) return;
+
+    this.totalPonto = 0;
+
     var mes = periodo.substr(0, 2);
     var ano = periodo.substr(2);
 
     this.service.getByUser(usuario, mes, ano).pipe(first()).subscribe((data: any) => {
+      var hSem = this.user.cargaSemana * 60; 
+      var hSab = this.user.cargaSabado * 60;          
+
+      for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        if(this.isWeekend(element.dataEntrada)) {          
+          data[index].resultado = data[index].saldo - hSab;
+          this.getResultado(data[index].resultado, data[index].saldo, data[index].autorizado, hSab);        
+        }else{
+          data[index].resultado = data[index].saldo - hSem;                    
+          this.getResultado(data[index].resultado, data[index].saldo, data[index].autorizado, hSem);        
+        }        
+      }
       this.pontos = data;
-
-      this.usuario = usuario;
+      
       this.periodo = periodo;
-
+      this.getTotalMes(mes, ano);
       this.spinner.hide();
+
+      console.log(this.totalPonto)
     });
   }
 
-  showMaps(localizacao) {
+  getResultado(resultado, saldo, autorizado, carga) { 
+    /** caso seja negativo, soma direto */
+    if(resultado < -10 && (autorizado == false || autorizado == null || autorizado == undefined)){
+      this.totalPonto += parseFloat(saldo);
+    }
+    /** caso positivo, verifica se for autorizado */
+    else if(resultado > 10 && autorizado == true){
+      this.totalPonto += parseFloat(saldo);
+    }
+    /** qualquer outra condição, pega o valor trabalhado */
+    else{
+      this.totalPonto += carga;
+    }
+  }
 
-    console.log(localizacao);
+  isWeekend(timestamp){
+    var data = new Date(timestamp);
+    var diaDaSemana = data.getDay();  
+
+    return diaDaSemana == 6 || diaDaSemana == 0;
+  }
+
+  getTotalMes(mes, ano) {
+    var dayLast = new Date(ano, parseInt(mes), 0);
+
+    var qtDias = 0;
+    var qtSab = 0;
+
+    for (let index = 1; index <= dayLast.getDate(); index++) {
+      var dCheck = new Date(ano, mes-1, index);
+
+      if(dCheck.getDay() == 6) qtSab++;
+      if(dCheck.getDay() != 6 && dCheck.getDay() != 0) qtDias++;
+    }
+
+    var hSab = this.user.cargaSabado * 60;    
+    var hSem = this.user.cargaSemana * 60;          
+    this.totalMes = (qtSab * hSab) + (qtDias * hSem);    
+  }
+
+  showMaps(localizacao) {
     localizacao = localizacao.split('|');
 
     this.lat = parseFloat(localizacao[1]);
@@ -85,7 +147,7 @@ export class ClockListComponent implements OnInit {
       if (result.isConfirmed) {
         this.service.autorizar(id).subscribe(data => {
           console.log(data);
-          this.getRegistros(this.usuario, this.periodo);
+          this.getRegistros(this.user.id, this.periodo);
         });
 
       }
